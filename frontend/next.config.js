0dbbfa8d5d3a0b2a4df0a33f1201667bfe45c949
Adam Wright <adamjosephwright@gmail.com>
2026-01-07 20:54:57 -0500

    Update Next.js config - build is already working

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Don't fail build on ESLint errors during production build
+    ignoreDuringBuilds: false,
+  },
 }
 
 module.exports = nextConfig
